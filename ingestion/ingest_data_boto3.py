6e7680c1b62aadda84852dd9484af9e74609cf78
dauk <dauk>
2025-07-30 21:25:15 +0000

    dev
    added load to bronze layer

---
+++ ingestion/ingest_data_boto3.py
@@ -0,0 +1,66 @@
+import boto3
+from datetime import datetime
+import os
+import sys
+
+sys.path.append("/Workspace/Users/dauuuk@gmail.com/hw/ingestion")
+
+from utils import return_source_config, return_sink_config
+
+
+SOURCE_ACCESS_KEY, SOURCE_SECRET_ACCESS_KEY, SOURCE_REGION = return_source_config()
+SINK_ACCESS_KEY, SINK_SECRET_ACCESS_KEY, SINK_REGION = return_sink_config()
+
+
+def ingest_data(
+    source_bucket_name: str, source_prefix: str, sink_bucket_name: str, sink_prefix: str
+) -> None:
+    source_s3_resource = boto3.resource(
+        "s3",
+        aws_access_key_id=SINK_ACCESS_KEY,
+        aws_secret_access_key=SINK_SECRET_ACCESS_KEY,
+        region_name=SINK_REGION,
+    )
+    sink_s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=SOURCE_ACCESS_KEY,
+        aws_secret_access_key=SOURCE_SECRET_ACCESS_KEY,
+        region_name=SOURCE_REGION,
+    )
+
+    # Creating file path based on current date
+    date_str = datetime.now().strftime("%Y/%m/%d")
+    timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    sink_path = f"{sink_prefix}/{date_str}"
+
+    bucket = source_s3_resource.Bucket(source_bucket_name)
+
+    for obj in bucket.objects.filter(Prefix=source_prefix):
+        source_key = obj.key
+
+        # Extract original filename and extension
+        filename = os.path.basename(source_key)
+        name, ext = os.path.splitext(filename)
+
+        # Append timestamp to filename
+        new_filename = f"{name}_{timestamp_str}{ext}"
+
+        destination_key = f"{sink_path}/{new_filename}"
+
+        copy_source = {"Bucket": source_bucket_name, "Key": source_key}
+        sink_s3_client.copy_object(
+            CopySource=copy_source, Bucket=sink_bucket_name, Key=destination_key
+        )
+
+        print(f"Copied {source_key} to {destination_key}")
+
+
+if __name__ == "__main__":
+    ingest_data(
+        source_bucket_name="romasdapublic",
+        source_prefix="hw-task/inputs/malware",
+        sink_bucket_name="hwbronze",
+        sink_prefix="malware",
+    )
+
+# TODO move copied files to processed
